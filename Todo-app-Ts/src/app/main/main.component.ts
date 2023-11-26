e18d870e585eca68619403e4952463a9e340a011
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-26 01:18:21 -0500

    create main component

---
+++ Todo-app-Ts/src/app/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
