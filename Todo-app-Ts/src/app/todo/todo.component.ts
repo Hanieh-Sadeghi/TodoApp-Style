005cf91d414b7ac4c8b2f61973494148c0a4f41f
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-26 01:14:02 -0500

    create todo app

---
+++ Todo-app-Ts/src/app/todo/todo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+}
